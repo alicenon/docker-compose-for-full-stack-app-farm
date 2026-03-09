cb846498171e6c96079eec3ecc5176b80b2f6ba9
Alejandro Lima <limasotoalejandro@gmail.com>
2026-03-09 21:27:52 +0100

    middleware origin

--- test-fastapi-app/main.py
+++ test-fastapi-app/main.py
@@ -6,7 +6,7 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
+    allow_origins=["http://100.55.8.196:9000"],
     allow_methods=["*"],
     allow_headers=["*"],
 )
