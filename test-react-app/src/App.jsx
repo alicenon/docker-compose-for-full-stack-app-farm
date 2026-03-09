48bea70d210fd02a2f4c3ad53dbebf558f7b5583
Alejandro Lima <limasotoalejandro@gmail.com>
2026-03-09 21:31:37 +0100

    fix error for fetch

--- test-react-app/src/App.jsx
+++ test-react-app/src/App.jsx
@@ -8,7 +8,7 @@ function App() {
   const [mensaje, setMensaje] = useState("");
 
   useEffect(() => {
-    fetch("http://100.24.26.86:8000/api/hello")
+    fetch("http://100.55.8.196:8000/api/hello")
       .then((res) => res.json())
       .then((data) => setMensaje(data.message));
   }, []);
